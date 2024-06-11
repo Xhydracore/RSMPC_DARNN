7006738a3cf0d8ceb5d6807c307b623851c4a82a
Xhydracore <sutrisnodeyndrawan@gmail.com>
2024-06-11 09:15:35 +0700

    setup project

---
+++ grbpath.sh
@@ -0,0 +1,7 @@
+#EDIT the paths to appropriate paths in your local machine
+export GUROBI_HOME="/home/ueesrg/gurobi1101/linux64"
+export GRB_LICENSE_FILE="/home/ueesrg/gurobi1101/gurobi.lic"
+export GUROBI_VERSION="110"
+export LD_LIBRARY_PATH="${GUROBI_HOME}/bin/lib"
+export PATH="${PATH}:${GUROBI_HOME}/bin"
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+import initialtools
+
+setuptools.setup(
+    name="infrastructure", 
+    version="0.1.0",
+    description='Robust Stochastic Model Predictive Control for Autonomous Vehicle Motion Planning',
+    author='Deyndrawan Sutrisno',
+    author_email='deyndrawansutrisno@students.unnes.ac.id',
+    packages=[
+        'infrastructure'
+    ],
+    install_requires=[]
+)
